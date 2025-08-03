05c15a77fbba896264462e1a37b66a117ab8d65e
cn-gourav <mrgusain558@gmail.com>
2025-08-03 21:13:11 +0530

    Craeted a revert Logic

--- Backend/controller/revert.js
+++ Backend/controller/revert.js
@@ -7,9 +7,19 @@ const copyFile = promisify(fs.copyFile);
 async function revert(commitID) {
     const repoPath = path.resolve(process.cwd(), ".apnaGit");
     const commitsPath = path.join(repoPath, "commits");
-    
+
     try {
-        
+        const commitDir = path.join(commitsPath, commitID);
+        const files = await readdir(commitDir);
+        const parentDir = path.resolve(repoPath, "..");
+
+        for (const file of files) {
+            await copyFile(
+                path.join(commitDir, file),
+                path.join(parentDir, file)
+            );
+        }
+        console.log(`Reverted ${file} to ${commitID}`);
     } catch (error) {
         console.error("Error reverting commit:", error);
         return;
